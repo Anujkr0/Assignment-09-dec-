ca3350c9449106958497e5047fbf48bc4db4b850
Anujkr0 <92168995+Anujkr0@users.noreply.github.com>
2021-12-12 18:16:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function mul() {
+
+
+    var input1ref = document.getElementById("input1").value;
+    var input2ref = document.getElementById("input2").value;
+    var input3ref = document.getElementById("input3").value;
+    var input4ref = document.getElementById("input4");
+    var btnref = document.getElementById("btn");
+    input4ref.value = input1ref * input2ref * input3ref;
+}
